a640b48300e0b62d374a16afbfb78d6306f7b8b0
yourhelp9 <info.yourhelp9@gmail.com>
2026-02-08 13:56:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,18 @@ import { defineConfig } from 'vite';
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { version } from "./package.json";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wordle/",
-  plugins: [svelte({
-    preprocess: vitePreprocess()
-  })],
+  base: "/", // ✅ Vercel ke liye
+  plugins: [
+    svelte({
+      preprocess: vitePreprocess()
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
         assetFileNames: `[name]-v${version}.[ext]`,
         entryFileNames: `[name]-v${version}.js`,
-        dir: "./dist",
       }
     }
   }
